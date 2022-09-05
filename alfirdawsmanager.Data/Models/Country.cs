8a4d7eb2b7e34c50b9db0a86ff1fec4ed155f835
Mohamed Achahbar <achahbar_mohamed@hotmail.com>
2022-09-06 00:20:14 +0200

    data type change

--- alfirdawsmanager.Data/Models/Country.cs
+++ alfirdawsmanager.Data/Models/Country.cs
@@ -13,7 +13,7 @@ namespace alfirdawsmanager.Data.Models
         public int CountryId { get; set; }
         public string? Name { get; set; }
         public string? Flag { get; set; }
-        public byte[]? Icon { get; set; }
+        public string? Icon { get; set; }
         public bool? Active { get; set; }
 
         public virtual ICollection<Region> Regions { get; set; }
--- alfirdawsmanager.Service/Models/CountryModel.cs
+++ alfirdawsmanager.Service/Models/CountryModel.cs
@@ -6,7 +6,7 @@ namespace alfirdawsmanager.Service.Models
         public int CountryId { get; set; }
         public string? Name { get; set; }
         public string? Flag { get; set; }
-        public byte[]? Icon { get; set; }
+        public string? Icon { get; set; }
         public bool? Active { get; set; }
         public int CountRegions { get; set; }
     }
