289501b18c853bac3117ff4b880334ca286b2d5c
Mohamed Achahbar <achahbar_mohamed@hotmail.com>
2022-09-08 14:40:32 +0200

    415  - Manage Regions of countries

--- alfirdawsmanager.Service/Models/CountryModel.cs
+++ alfirdawsmanager.Service/Models/CountryModel.cs
@@ -9,6 +9,13 @@ namespace alfirdawsmanager.Service.Models
         public string? Icon { get; set; }
         public bool? Active { get; set; }
         public int CountRegions { get; set; }
+        public List<RegionModel>? Regions { get; set; }
+    }
+
+    public class RegionModel
+    {
+        public int? RegionId { get; set; }
+        public string? Name { get; set; }
     }
 }
 
